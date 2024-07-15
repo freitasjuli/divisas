23a3b45b188bfb3edb926511fde1fe00b4ea6229
arangurenjulian <arangurenjulian659@gmail.com>
2024-07-14 20:23:26 -0500

    actualizacion error

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,20 @@ const convertirValor = function(){
     const valor = document.querySelector('#valor').value;
     const moneda = document.querySelector('.moneda').value;
 
-    const valorCombertido = valor / datos[moneda].valor;
+    try
+    {
+        const valorCombertido = valor / datos[moneda].valor;
 
-    const texto =document.querySelector('.resultado');
-    if(moneda === 'dolar'){
-        divisa = "Dolares";
-    }else{
-        divisa = "Euros";
+        if(moneda === 'dolar'){
+            divisa = "Dolares";
+        }else{
+            divisa = "Euros";
+        }
+        document.querySelector('.resultado').innerHTML= `$ ${valor} pesos equibalen a ${valorCombertido} ${divisa}`;
     }
-    texto.innerHTML= `$ ${valor} pesos equibalen a ${valorCombertido} ${divisa}`;
+    catch(error){
+        document.querySelector('.resultado').textContent='No fue posible la conversion de la divisa';
+    }
 }
 
 obtenerdatos();
